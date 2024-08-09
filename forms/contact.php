193c37ea6db8995533812743e5c74fdb6030a65b
pakanonymousofficial <farasat@pakanonymous.org>
2024-08-10 00:38:42 +0500

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -12,7 +12,7 @@
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Server Are Also Coming Soon!');
   }
 
   $contact = new PHP_Email_Form;
