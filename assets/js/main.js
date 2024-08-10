de06003acfaaae10cc2605113388b7ce0e9f1c43
pakanonymousofficial <farasat@pakanonymous.org>
2024-08-10 12:18:10 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,7 +103,7 @@
     const days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeleft % (100 * 60)) / 1000);
+    const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
     countDownItem.querySelector('.count-days').innerHTML = days;
     countDownItem.querySelector('.count-hours').innerHTML = hours;
